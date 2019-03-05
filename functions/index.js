95b9dbacb3aaad0566385f5fd119febdc86863ae
unknown <damian_morales_utn@hotmail.com>
2019-03-05 17:31:52 -0300

    added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+var engines = require('consolidate');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+
+// Routes
+app.get('/prueba',function(req,res){
+
+    res.render('prueba')
+});
+
+ exports.app = functions.https.onRequest(app);
+ 
