aed9ebc5e918c98d85da8ce2e37845763dd685d1
unknown <damian_morales_utn@hotmail.com>
2019-03-11 17:42:25 -0300

    added firebase admin

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
+var admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 var engines = require('consolidate');
 
+
+
+var serviceAccount = require('./config/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sortegramm.firebaseio.com"
+});
+
+var db = admin.firestore();
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
@@ -11,8 +22,37 @@ app.set('view engine', 'hbs');
 // Routes
 app.get('/prueba',function(req,res){
 
+    
+
     res.render('prueba')
 });
 
- exports.app = functions.https.onRequest(app);
+
+app.get('/dashboard',function(req,res){
+
+    var sorteos = []
+    db.collection('Sorteos').get()
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          console.log(doc.id, '=>', doc.data());
+          sorteos.push(doc.data())
+        });
+      
+      return sorteos
+    }).then((sorteos)=>{
+
+       // res.render('dashboard',{sorts: [{Categoria_Producto: "Accesorios"},{Categoria_Producto: "Accesorios"} ]})
+        console.log(sorteos)
+        res.render('dashboard',{sorts: sorteos})
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+
+    //console.log(sorteos)
+   
+});
+
+
+exports.app = functions.https.onRequest(app);
  
--- public/assets/js/validationForm.js
+++ public/assets/js/validationForm.js
@@ -1,2 +1,25 @@
 
 // validacion 
+/** 
+let c1 = 0;
+let c2 = 0;
+
+$('.entrada').keyup( function(){
+
+    if ($('.has-error').length > 0 && c1 == 0){
+        console.log('error')
+    
+        $($('.has-error').children()[2]).removeClass('bmd-help')
+        c1++; 
+    } else {
+        c1++;
+        console.log('no error')
+        $($('.has-error').children()[2]).addClass('bmd-help')
+        
+    }
+    
+
+})
+*/
+
+$('#categoriaProd').rules('add', {required: true,messeges: {required:'test'}})
