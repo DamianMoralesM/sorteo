26895a8582f24b9b17db12d8194a0418c7c4dac1
unknown <damian_morales_utn@hotmail.com>
2019-03-15 00:46:59 -0300

    unplash added to step1

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,18 @@ const express = require('express');
 const app = express();
 var engines = require('consolidate');
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+const Unsplash = require('unsplash-js').default;
+const toJson =  require('unsplash-js').toJson;
+
+
+const unsplash = new Unsplash({
+  applicationId: "30bec4c9a180eea6631123f50433fe7d3c689d4aa899598a194cb67cb86aca11",
+  secret: "e4c72929d40c80dfbbfc5fb28cdd2ab49d8e5baada1ffed2633448cbdc94ebb7"
+});
+
+
 
 
 var serviceAccount = require('./config/serviceAccountKey.json');
@@ -27,6 +39,27 @@ app.get('/prueba',function(req,res){
     res.render('prueba')
 });
 
+app.get('/index', function(req,res){
+  var salida = []
+  unsplash.search.photos("food", 1,10)
+  .then(toJson)
+  .then(response => {
+  
+    
+    (response.results).forEach(element => {
+      salida.push(element.urls.thumb)        
+  });
+  //console.log(salida)
+    
+    res.render('publicarSorteo', {imagenes: salida})
+  });
+
+
+  
+
+   
+
+})
 
 app.get('/dashboard',function(req,res){
 
--- functions/unplashTest.js
+++ functions/unplashTest.js
@@ -9,11 +9,16 @@ const unsplash = new Unsplash({
   applicationId: "30bec4c9a180eea6631123f50433fe7d3c689d4aa899598a194cb67cb86aca11",
   secret: "e4c72929d40c80dfbbfc5fb28cdd2ab49d8e5baada1ffed2633448cbdc94ebb7"
 });
-
+var salida = []
 unsplash.search.photos("dogs", 1,10)
   .then(toJson)
   .then(response => {
-    console.log(response.results);
-      return response
+    
+   
+      (response.results).forEach(element => {
+          salida.push(element.urls.thumb)        
+      });
+
+      console.log( salida )
    
   });
